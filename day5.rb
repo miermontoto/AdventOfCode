0e9aee9c46885319af294af0f5d3072997a5fc38
miermontoto <mier@mier.info>
2022-12-05 18:08:50 +0100

    day 5: finish

--- day5.rb
+++ day5.rb
@@ -1,30 +1,55 @@
 def read_grid(input)
-    info = ""
-    input.each_line do |line|
-        break if line === "\n"
-        info += line
-    end
-    info = info.lines
-    cols = info.pop.strip().split('   ').length
-    rows = info.length
+    lines = input.lines
+    cols = lines.pop.strip().split('   ').length
+    grid = Array.new(cols) { [] }
 
+    lines.each do |line|
+        (1..line.length).step(4) do |pos|
+            box = line[pos]
+            grid[(pos - 1) / 4].push(box) unless box === ' '
+        end
+    end
+
+    grid.each(&:reverse!)
+end
+
+def read_commands(input)
+    commands = []
+
+    input.each_line do |line|
+        line = line.strip.split(' ').map(&:to_i).reject(&:zero?)
+        commands << [line[0], line[1] - 1, line[2] - 1]
+    end
+
+    return commands
+end
+
+def read_info(input)
+    grid, commands = input.split("\n\n")
+    return read_grid(grid), read_commands(commands)
 end
 
 def star_one(input)
-    grid = read_grid(input)
+    grid, commands = read_info(input)
 
-    input.each_line do |line|
-        next
+    commands.each do |iter, src, dest|
+        iter.times { grid[dest] << grid[src].pop }
     end
 
-    return nil
+    grid.map(&:last).join
 end
 
 def star_two(input)
+    grid, commands = read_info(input)
 
+    commands.each do |iter, src, dest|
+        grid[dest].concat(grid[src].pop(iter))
+    end
+
+    grid.map(&:last).join
 end
 
-input = File.read("input/5_mini")
+input = File.read("input/5")
 print "star one: ", star_one(input)
 puts
 print "star two: ", star_two(input)
