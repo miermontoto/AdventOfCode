e27ddc05fc95f87a10f76c6307bab928b0f49b64
miermontoto <UO283319@uniovi.es>
2022-12-05 14:14:17 +0100

    day 5: progress

--- day5.rb
+++ day5.rb
@@ -1,5 +1,23 @@
-def star_one(input)
+def read_grid(input)
+    info = ""
+    input.each_line do |line|
+        break if line === "\n"
+        info += line
+    end
+    info = info.lines
+    cols = info.pop.strip().split('   ').length
+    rows = info.length
 
+end
+
+def star_one(input)
+    grid = read_grid(input)
+
+    input.each_line do |line|
+        next
+    end
+
+    return nil
 end
 
 def star_two(input)
