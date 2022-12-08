e1add2dfe9e3f14d8a67f0df2a2d563f132f0491
miermontoto <mier@mier.info>
2022-12-08 17:50:56 +0100

    day 8: init

---
+++ day8.rb
@@ -0,0 +1,11 @@
+# Advent of Code 2022, day 8
+
+def star_one(input)
+end
+
+def star_two(input)
+end
+
+input = File.read("input/8")
+puts "star one: #{star_one(input)}"
+puts "star two: #{star_two(input)}"
