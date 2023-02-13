e7b541a837f471af7000eab0a36820ada9c8c7c5
miermontoto <UO283319@uniovi.es>
2023-02-13 12:41:49 +0100

    star one: day9

--- day9.rb
+++ day9.rb
@@ -1,10 +1,10 @@
 # Advent of Code 2022, day 9
 
 class Piece
-    def initialize(grid)
+    def initialize()
         @x = 0
-        @y = grid.first.size - 1
-        @log = []
+        @y = 0
+        @log = [[@x, @y]]
     end
 
     def x()
@@ -20,15 +20,15 @@ class Piece
     end
 
     def touching?(other)
-        (other.x - self.x).abs <= 1 or (other.y - self.y).abs <= 1
+        (other.x - self.x).abs <= 1 and (other.y - self.y).abs <= 1
     end
 
     def move(dir)
         case dir
         when 'U'
-            @y = self.y - 1
-        when 'D'
             @y = self.y + 1
+        when 'D'
+            @y = self.y - 1
         when 'L'
             @x = self.x - 1
         when 'R'
@@ -36,7 +36,6 @@ class Piece
         end
 
         @log.push([self.x, self.y])
-        print "Pushing [#{self.x},#{self.y}]\n"
     end
 
     def set_pos(x, y)
@@ -47,9 +46,8 @@ end
 
 
 def star_one(input)
-    grid = Array.new(4, Array.new(4))
-    head = Piece.new(grid)
-    tail = Piece.new(grid)
+    head = Piece.new()
+    tail = Piece.new()
 
     possible_dir = ['U', 'D', 'L', 'R']
     reverse_dir = ['D', 'U', 'R', 'L']
@@ -66,13 +64,41 @@ def star_one(input)
             end
         end
     end
-    puts head.log
     tail.log.uniq.count
 end
 
 def star_two(input)
+    knots = 10
+    pieces = []
+    possible_dir = ['U', 'D', 'L', 'R']
+    reverse_dir = ['D', 'U', 'R', 'L']
+
+    knots.times do
+        pieces.push(Piece.new())
+    end
+
+    input.each_line do |line|
+        dir = line.split.first
+        times = line.split.last.to_i
+
+        times.times do
+            pieces.each_with_index do |piece, i|
+                if i == 0
+                    piece.move(dir)
+                    next
+                end
+
+                if not piece.touching?(pieces[i - 1])
+                    piece.set_pos(pieces[i - 1].x, pieces[i - 1].y)
+                    piece.move(reverse_dir[possible_dir.index(dir)])
+                end
+            end
+        end
+    end
+
+    pieces.last.log.uniq.count
 end
 
 input = File.read("input/9")
 puts "star one: #{star_one(input)}"
-puts "star two: #{star_two(input)}"
+puts "star two: #{star_two(input)}" # TODO: fix
