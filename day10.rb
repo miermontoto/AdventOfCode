6cfb5e77bb91e6631a8405bf0b198c153daefd12
mier <mier@MacBook-Air-de-Francisco.local>
2023-02-16 13:53:19 +0100

    init day 10

--- day10.rb
+++ day10.rb
@@ -1,6 +1,35 @@
 # Advent of Code 2022, day 10
 
 def star_one(input)
+    reg = 1
+    cycle = 1
+    target = 20
+    sum = 0
+
+    input.each_line do |line|
+        instruction = line.split.first
+        number = line.split.last.to_i
+
+        case instruction
+        when 'noop'
+            add = 1
+        when 'addx'
+            reg += number
+            add = 2
+        end
+
+        cycle += add
+        target -= add
+        if target <= 0 then
+            if instruction == 'addx' and target == -1 then
+                sum += (reg - number) * cycle
+            else
+                sum += reg * cycle
+            end
+            target = 40
+        end
+    end
+    sum
 end
 
 def star_two(input)
